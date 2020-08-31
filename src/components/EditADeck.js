dfe14d63216222cec3a0905d61f5c903357c4799
sdennett55@gmail.com <sdennett55@gmail.com>
2020-08-30 20:44:49 -0400

    make whole button on table clickable

--- src/components/EditADeck.js
+++ src/components/EditADeck.js
@@ -584,7 +584,7 @@ const RegularButton = styled.button`
   transition: color 0.25s;
   background: transparent;
   color: #fff; 
-  padding: 1px 6px;
+  padding: 1px .5em;
 
   @media (hover) {
     &:hover {
