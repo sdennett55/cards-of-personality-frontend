f3d8e221d94f49cb1973b0fb33939ad3f92aaa33
Steven Dennett <sdennett55@gmail.com>
2022-12-17 09:18:38 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const SERVER_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3001"
-    : "https://cards-against-steve.herokuapp.com";
+    : "cards-of-personality-backend-production.up.railway.app";
 
 export const CLIENT_URL =
   process.env.NODE_ENV === "development"
